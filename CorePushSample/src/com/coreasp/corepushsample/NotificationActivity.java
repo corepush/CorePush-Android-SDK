356e02635aa7857ac443d19cc46d13d5421f403e
corepush <info@core-asp.comopen>
2012-09-18 15:10:30 +0900

    通知から起動するアクティビティで通知情報を取得できるように修正しました。

--- CorePushSample/src/com/coreasp/corepushsample/NotificationActivity.java
+++ CorePushSample/src/com/coreasp/corepushsample/NotificationActivity.java
@@ -1,6 +1,7 @@
 package com.coreasp.corepushsample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.coreasp.CorePushManager;
@@ -18,10 +19,10 @@ public class NotificationActivity extends Activity {
         
         //設定キーを指定
         manager.setConfigKey("XXXXXXXXXXXXXXXXX");
-
+        
         //senderId(projectId)を指定
         manager.setSenderId("1234567890");
-
+        
         //通知から起動するアクティビティを指定
         manager.setActivity(NotificationActivity.class);
         
@@ -33,7 +34,14 @@ public class NotificationActivity extends Activity {
         
         ////CORE PUSHからデバイストークンを削除
         //manager.removeToken(this);
+        
+        // 通知センターから起動時に通知パラメータを取得
+        Intent intent = getIntent();
+        
+        String date = manager.getDate(intent);
+        String title = manager.getTitle(intent);
+        String message = manager.getMessage(intent);
+        String url = manager.getUrl(intent);
     }
     
 }
-
